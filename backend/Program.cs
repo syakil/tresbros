7d70f4c93724039d3e771416f01e5c23c3430337
syakil <syakilahmadginanjar@gmail.com>
2026-06-25 22:52:45 +0700

    fix: disable HttpsRedirection in production Docker container

--- backend/Program.cs
+++ backend/Program.cs
@@ -67,9 +67,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
